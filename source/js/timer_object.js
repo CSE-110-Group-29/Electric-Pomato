9876f503041c07c59a5b82b8b78a212a3fda4fd1
DonaldWolfson <dwolfson486@gmail.com>
2021-02-03 10:49:41 -0800

    initial commit of file skeleton

---
+++ source/js/timer_object.js
@@ -0,0 +1,8 @@
+/* *
+ * Author: (Put your name here if you're the first assignee)
+ * Updated By: (Any names of people who've done some editing of the file)
+ * Date: (Last updated)
+ * Github Issue: https://github.com/DonaldWolfson/cse110-w21-group29/issues/12
+ * */
+
+ // TODO:
