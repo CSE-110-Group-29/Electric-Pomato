012e82beb6f41458c702b125003d2a1fc4d1e450
amansourian <mansourianarman@gmail.com>
2021-02-08 17:02:37 -0800

    cleaned up package.json files and eslintrc

--- source/.eslintrc.js
+++ source/.eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: 'airbnb-base',
+  env: {
+    // To allow console variable.
+    browser: true,
+  },
+  rules: {
+    // Because browser doesn't automatically add the file extensions.
+    'import/extensions': [
+      'error',
+      'always',
+    ],
+    // To allow console.log().
+    'no-console': 'off',
+  },
 };
+
