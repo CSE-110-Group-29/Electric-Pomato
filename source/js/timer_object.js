ab8616238789d23429a83b6f6cecba6eaaf94754
Enrique Gan <PitEG@github.com>
2021-02-12 16:47:46 -0800

    linted

--- source/js/timer_object.js
+++ source/js/timer_object.js
@@ -5,72 +5,68 @@
  * Github Issue: https://github.com/DonaldWolfson/cse110-w21-group29/issues/12
  * */
 
- // TODO:
+// TODO:
 class Timer {
-    /* *
+  /* *
      * Constructor for the timer object.
      * @input minutes: Numerical value of maximum minutes.
      * @input seconds: Numerical value of maximum seconds.
-     * @input timerElement: The timer on the document to update time for 
+     * @input timerElement: The timer on the document to update time for
      * */
-    constructor(minutes, seconds, timerElement) { 
-        //time = new Date();
-        this.minutes = minutes;
-        this.seconds = seconds;
-        this.timerElement = timerElement;
-    }
+  constructor(minutes, seconds, timerElement) {
+    // time = new Date();
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.timerElement = timerElement;
+  }
 
-    /* *
+  /* *
      * Starts ticking the timer. Timer cannot be reset to original time after ticking.
-     * Note: This doesn't not check system date/time. It ticks at 1 second 
+     * Note: This doesn't not check system date/time. It ticks at 1 second
      * intervals, possibly being ahead or behind by negligible amounts.
      * */
-    startTimer() {
-        return new Promise(resolve => {
-            let countdown = setInterval(() => {
-                console.log(this.parseMinutes() + ":" + this.parseSeconds());
-                if(this.seconds == 0 && this.minutes != 0) {
-                    this.minutes --;
-                    this.seconds = 60;
-                } 
-                else if (this.seconds == 0 && this.minutes == 0) {
-                    resolve();
-                    clearInterval(countdown);
-                }
-                this.seconds --;
-            }, 1000);
-        });
-    }
+  startTimer() {
+    return new Promise((resolve) => {
+      const countdown = setInterval(() => {
+        console.log(`${this.parseMinutes()}:${this.parseSeconds()}`);
+        if (this.seconds === 0 && this.minutes !== 0) {
+          this.minutes -= 1;
+          this.seconds = 60;
+        } else if (this.seconds === 0 && this.minutes === 0) {
+          resolve();
+          clearInterval(countdown);
+        }
+        this.seconds -= 1;
+      }, 1000);
+    });
+  }
 
-    /* *
+  /* *
      * Returns a string representing the minutes left with the format "MM".
      * ie: If 25 minuts are left, "25". If 9 minutes are left "09"
      * @returns {String} Minutes Left
      * */
-    parseMinutes() {
-        if(this.minutes < 10) 
-            return "0" + String(this.minutes);
-        return String(this.minutes);
-    }
+  parseMinutes() {
+    if (this.minutes < 10) { return `0${String(this.minutes)}`; }
+    return String(this.minutes);
+  }
 
-    /* *
+  /* *
      * Returns a string representing the seconds left with the format "SS".
      * ie: If 25 seconds are left, "25". If 9 seconds are left "09"
      * @returns {String} Seconds Left
      * */
-    parseSeconds() {
-        if(this.seconds == 60)
-            return "00";
-        else if(this.seconds < 10)
-            return "0" + String(this.seconds);
-        return String(this.seconds)
-    }
+  parseSeconds() {
+    if (this.seconds === 60) return '00';
+    if (this.seconds < 10) return `0${String(this.seconds)}`;
+    return String(this.seconds);
+  }
 }
 
 async function sessionTest() {
-    const timer = new Timer(0, 5, null);
-    await timer.startTimer();
-    console.log("DONE");    
+  const timer = new Timer(0, 5, null);
+  await timer.startTimer();
+  console.log('DONE');
 }
 
 sessionTest();
