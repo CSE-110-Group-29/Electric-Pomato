4bb3648548014e200e8647bdb164dbd81af3dd59
DonaldWolfson <dwolfson486@gmail.com>
2021-02-12 18:59:02 -0800

    Updated session ESLint errors

--- source/reference/session.js
+++ source/reference/session.js
@@ -11,14 +11,12 @@
 //  *  IF YOU DON'T LIKE THE NAMES OF ANYTHING NOW IS THE TIME TO CHANGE THEM!
 //  *      I promise I won't be offended :)
 //  * */
-
 // /** Global Values: (TODO)                                                  **/
 // //     Will hold DOM data on HTML elements (buttons, forms, etc.)
 // //     The ID's and names aren't final, and don't connect to anything in HTML.
 // //     Just made them in hopes of simulating some processes better.
 // //     These are defined based on the design Teresa made, check the Project 
 // //     Pitch do for context.
-
 // // Figure 1:
 // let returningUserBtn = document.getElementById("returningUserBtn");
 // let newUserNameForm = document.getElementById("newUserNameForm");
@@ -45,50 +43,34 @@
 // let continueFinalBreakBtn = document.getElementById("continueFinalBreakBtn");
 // // Figure 6:
 // let recordsDisplay = document.getElementById("recordsDisplay")
-
 // /** Event Listeners: (TODO)                                                  **/
 // // Add event listeners for the elements above, and map them to helper functions.
 // startFirstTaskBtn.addEventListener("click", startTaskHandler);
-
-
 // /** Helper Methods: (TODO)                                                   **/
 // //      Based on the System Diagram: https://github.com/DonaldWolfson/cse110-w21-group29/blob/main/specs/brainstorm/System%20Diagram.jpg
-
 // // Timer Related Helper Methods:
 // //  Unsure where the front end values ()
-
 // function startTaskHandler() {
 //     // TODO: Start a generic 25:00 timer.
 // }
-
 // function startShortBreakHandler() {
 //     // TODO: Start a short break 5:00 timer.
 // }
-
 // function startLongBreakHandler() {
 //     // TODO: Start a long break 25:00 timer.
 // }
-
 // function updateTimerDisplay() {
 //     // TODO: Update timer display every second.
 // }
-
 // // Task List Related Helper Methods:
-
 // function addTaskHandler() {
 //     // TODO: Add a new task to the taskList object.
 // }
-
 // function removeTaskHandler() {
 //     // TODO: Remove a task from the taskList object.
 // }
-
 // function editTaskHandler() {
 //     // TODO:"Edit an existing task.
 // }
-
 // // HTML related Helper Methods:
-
-
-
 // // Misc. Helper Methods:
