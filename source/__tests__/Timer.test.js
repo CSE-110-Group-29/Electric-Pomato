58b52ffff291f7085f7c6a7742404d693fbf6807
DonaldWolfson <dwolfson486@gmail.com>
2021-02-15 20:05:42 -0800

    Minor fix

--- source/__tests__/Timer.test.js
+++ source/__tests__/Timer.test.js
@@ -1,6 +1,6 @@
 const { Timer } = require('../js/Timer');
 
-test('Check Timer\'s Initialized Values', () => {
+test('Check Timers Initialized Values', () => {
   const FiveSecondTimer = new Timer(0, 5, null);
   expect(FiveSecondTimer.parseMinutes()).toBe('00');
   expect(FiveSecondTimer.parseSeconds()).toBe('05');
