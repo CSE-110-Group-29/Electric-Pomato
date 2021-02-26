15403b33f88d6185467eeb73f536fc5fe818c6b5
Annika Hatcher <anhatche@ucsd.edu>
2021-02-25 18:47:30 -0800

    added sliding task list

--- source/js/app.js
+++ source/js/app.js
@@ -3,4 +3,19 @@ import ViewOnlyTaskList from './ViewOnlyTaskList.js';
 
 const e = new EditableTaskList();
 const v = new ViewOnlyTaskList();
-document.querySelector('.task-list-container').appendChild([e, v][Math.floor(Math.random() * 2)]);
+document.querySelector('.task-list-container').appendChild(v);
+
+const taskList = document.querySelector('.task-list-container');
+const dropToggle = document.getElementById('toggle');
+
+taskList.addEventListener('click', () => {
+  if (dropToggle.checked === true) {
+    taskList.classList.add('hidden');
+    taskList.classList.remove('showing');
+    dropToggle.checked = false;
+  } else {
+    taskList.classList.add('showing');
+    taskList.classList.remove('hidden');
+    dropToggle.checked = true;
+  }
+});
